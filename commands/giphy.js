1f749226669c7857673c52566759af16bbfcf2a3
ItsNotOlykir <olykirofficial@gmail.com>
2018-06-04 21:51:57 -0500

    Updated params.join

--- commands/giphy.js
+++ commands/giphy.js
@@ -1,11 +1,18 @@
 const snek = require('snekfetch')
 exports.run = (client, message, params) => {
-    const search = params.join(" ")
-    snek.get(`http://api.giphy.com/v1/stickers/search?q=${search}&api_key=${client.settings.giphyKey}`)
-    .then(r => message.channel.send({embed: {title: `Success!`, image: {url: `${r.body.data[0].images.preview_gif.url}`}}}));
+	const search = params.join("+")
+	snek.get(`http://api.giphy.com/v1/stickers/search?q=${search}&api_key=${client.settings.giphyKey}`)
+		.then(r => message.channel.send({
+			embed: {
+				title: `Success!`,
+				image: {
+					url: `${r.body.data[0].images.preview_gif.url}`
+				}
+			}
+		}));
 }
 
 exports.info = {
-    name: "giphy",
-    desc: 'Search giphy'
+	name: "giphy",
+	desc: 'Search giphy'
 }
