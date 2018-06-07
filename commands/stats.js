72b24bb01bc3d9b3da721615dd1a2cda94cf1e05
ItsNotOlykir <olykirofficial@gmail.com>
2018-06-07 13:07:59 -0500

    updated stats

--- commands/stats.js
+++ commands/stats.js
@@ -6,7 +6,10 @@ exports.run = (client, msg, args) => {
 	const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
 
 	const embed = new Discord.RichEmbed()
-		.setColor(2197497).setTitle(`Bot's status!`).setURL("https://discordapp.com/oauth2/authorize?client_id=452255697032183819&scope=bot&permissions=67161152").setThumbnail(client.user.displayAvatarURL).addField("Mem Usage", `**${ (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`).addField("Uptime", `**${duration}**`)
+		.setColor(2197497).setTitle(`Bot's status!`).setURL(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=67161152`)
+		.setThumbnail(client.user.displayAvatarURL)
+		.addField("Mem Usage", `**${ (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`)
+		.addField("Uptime", `**${duration}**`)
 		.addField("Users", `**${client.users.size - 1}**`)
 		.addField(`Servers`, `**${client.guilds.size.toLocaleString()}**`)
 		.addField(`Channels`, `**${client.channels.size.toLocaleString()}**`)
