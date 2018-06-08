c702ff88dbb88939958246e30f07a6503f7bbbbf
ItsNotOlykir <olykirofficial@gmail.com>
2018-06-08 12:22:00 -0500

    tweak

--- commands/announce.js
+++ commands/announce.js
@@ -1,14 +1,14 @@
 exports.run = (client, message, params) => {
 	if (!message.member.hasPermission("PIN_MESSAGES")) {
-		message.channel.send('You cannot use this command!')
+		message.channel.send("You cannot use this command!");
 	} else {
-		const messageText = params.join(" ")
+		const messageText = params.join(" ");
 		message.delete()
-		message.channel.send(`Annoucment:\n**${messageText}**`).then(p => p.pin())
+		message.channel.send(`Annoucment:\n**${messageText}**`).then(p => p.pin());
 	}
 }
 exports.info = {
-	name: 'announce',
-	desc: 'Annouce something to the server',
-	use: `-announce \'message\'`
+	name: "announce",
+	desc: "Annouce something to the server",
+	use: `-announce \"message\"`
 }
