2ebbd4b6ffdeee9783cfbfa170ca6a18e9b96d8d
ItsNotOlykir <olykirofficial@gmail.com>
2018-07-09 12:47:34 -0500

    RAINBOW

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,3 @@ module.exports = client => {
 	console.log("I'm Online!");
 	// Set the status of the bot
 	client.user.setActivity(`-help || ${client.users.size - 1} users!`);
-	// setInterval(function() {
-	// 	const RainbowRole = client.guilds
-	// 		.get("452370435498311691")
-	// 		.roles.get("452721663990038528");
-	// 	RainbowRole.setColor(Math.floor(Math.random() * 16777215));
-	// }, 8000);
-};
