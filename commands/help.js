ed3491769332e5cdd49d6f172cd48e45d4ffb995
ItsNotOlykir <olykirofficial@gmail.com>
2018-08-14 02:14:50 -0500

    bug fix

--- commands/help.js
+++ commands/help.js
@@ -31,7 +31,10 @@ exports.run = (client, message, params) => {
 			url: client.supportServer,
 			color: 2197497,
 			description: `${client.commands.map(c => `**${client.settings.prefix}${c.info.name}**${ " ".repeat(longest - c.info.name.length)}   || ${c.info.desc}`).join("\n")}`,
-			footer: `Need support? Click [here](${client.supportServer})`
+			footer: {
+				//icon_url: client.user.avatarURL,
+				text: `Need support? Click [here](${client.supportServer})`
+			  }
 		}
 	})
 
