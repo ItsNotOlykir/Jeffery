a4fcdbc236d3ddfd6252baa01c6f8c55ca17a87a
ItsNotOlykir <olykirofficial@gmail.com>
2018-08-15 00:33:13 -0500

    errors

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const snekfetch = require("snekfetch");
 require("./util/eventLoader")(client);
 // Set client vars
 client.settings = settings;
-client.commands = new Discord.Collection();
+// client.commands = new Discord.Collection();
+client.commands = new Array
 client.functions = new Discord.Collection();
 client.supportServer = settings.SupportServer
 client.ownerID = settings.ownerID
